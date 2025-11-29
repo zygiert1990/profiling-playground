486dd0a0f54cd6187775a2207bf3a3eb73dc26e9
Michał Zyga <michal.zyga@softwaremill.com>
2025-11-29 05:30:42 +0100

    change example to include also getters

--- src/main/java/org/zygiert/Main.java
+++ src/main/java/org/zygiert/Main.java
@@ -5,10 +5,11 @@ public class Main {
     static void main(String[] args) throws InterruptedException {
         var nrOfIterations = Integer.parseInt(args[0]);
         var sleepInMillis = Integer.parseInt(args[1]);
+        var argumentHolder = new ArgumentsHolder(nrOfIterations, sleepInMillis);
         var sum = 0;
-        for (int i = 1; i <= nrOfIterations; i++) {
+        for (int i = 1; i <= argumentHolder.getNrOfIterations(); i++) {
             IO.println("This is: " + i + " iteration");
-            Thread.sleep(sleepInMillis);
+            Thread.sleep(resolveSleepInMillis(i, argumentHolder.getSleepInMillis()));
             sum += resolveNumber(i);
         }
         IO.println("My fantastic sum is: " + sum);
@@ -19,6 +20,33 @@ public class Main {
             return i;
         } else {
             return i * 2;
+        }
+    }
+
+    private static int resolveSleepInMillis(int i, int sleepInMillis) {
+        if (i < 20000) {
+            return sleepInMillis;
+        } else {
+            return sleepInMillis / 10;
+        }
+    }
+
+    private static final class ArgumentsHolder {
+
+        private final int nrOfIterations;
+        private final int sleepInMillis;
+
+        private ArgumentsHolder(int nrOfIterations, int sleepInMillis) {
+            this.nrOfIterations = nrOfIterations;
+            this.sleepInMillis = sleepInMillis;
+        }
+
+        private int getNrOfIterations() {
+            return nrOfIterations;
+        }
+
+        private int getSleepInMillis() {
+            return sleepInMillis;
         }
     }
 
